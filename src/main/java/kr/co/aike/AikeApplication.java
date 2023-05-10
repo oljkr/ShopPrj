d35c0b895ec7db0fb07c49a2c61051c3d5e0fba8
oljkr <zaoljkr@gmail.com>
2023-05-11 07:29:55 +0900

    처음 설정

---
+++ src/main/java/kr/co/aike/AikeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.aike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/aike/AikeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.aike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
